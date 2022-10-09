08575eae8a98f966bfe37d7a60f5673d002e12b1
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-09 21:08:59 +0300

    added code in Task5

--- Task5.py
+++ Task5.py
@@ -2,3 +2,10 @@
 #  Пример:
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
+import math
+x1 = float(input('Введите х1: '))
+y1 = float(input('Введите y1: '))
+x2 = float(input('Введите x2: '))
+y2 = float(input('Введите y2: '))
+AB = round(math.sqrt((x2 - x1)**2 + (y2 - y1)**2), 2)
+print('Расстояние между координатами точек ', AB )
