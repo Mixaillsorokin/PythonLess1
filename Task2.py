b105cdc58a39f6b062a39570097058d5bcd4dc1e
Sorokin Mikhail <ip.sorokin1738@mail.ru>
2022-10-13 01:30:15 +0300

    added code in Task2

--- Task2.py
+++ Task2.py
@@ -1,1 +1,11 @@
 #  Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input('X'))
+
+y = int(input('Y'))
+
+z = int(input('Z'))
+if not(x or y or z) == (not x) and (not y) and (not z):
+    print('True')
+else:
+    print('False')
